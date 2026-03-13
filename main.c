702f5712d3b3f004a4c2135432d1e1f45fe3c931
Noeliaarosquita <a2023110475@estudiantes.upsa.edu.bo>
2026-03-13 12:18:15 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() {
+    float lab_1, lab_2, lab_3,examen_teorico,proyecto_final,promedio;
+    printf("---Promedio del estudiante---\n");
+
+    //Entrada de datos
+    printf("Ingrese nota lab1: " );
+    scanf("%f",&lab_1);
+       printf("Ingrese nota lab2 " );
+    scanf("%f",&lab_2);
+       printf("Ingrese nota lab3: " );
+    scanf("%f",&lab_3);
+
+    printf("Ingrese la nota del examen teorico: ");
+    scanf("%f",&examen_teorico);
+    printf("Ingrese la nota del proyecto final");
+    scanf("%f",&proyecto_final);
+
+  promedio= (lab_1* 0.15)+(lab_2* 0.15)+(lab_3* 0.15)+(examen_teorico*0.35)+(proyecto_final*0.20);
+  printf("-----------------------------------\n");
+  printf("Tu promedio es: %.2f\n",promedio);
+  if (promedio>=51) {printf("felicidades aprobaste\n");}
+  else {printf("reprobaste\n");}
+  
+  
+  
+  return 0;
+
+
+}
+
